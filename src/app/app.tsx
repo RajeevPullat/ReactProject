4b8a8ab0444d6d41925d51ea351bcda922c08f46
Rajeev <rajeev.raj.p@gmail.com>
2019-02-17 16:42:04 +0530

    Image Handling added inside Components

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+const yomanimg= require("../public/img/yeoman-003.png");
 
 export default class App extends React.Component{
     render(){
         return(
-            <h1>Hello React..!</h1>
+            <div className="appContainer">
+                <h1>Hello React..!</h1>
+                <img src={yomanimg} alt="logo"></img>
+            </div>
         );
     }
 }
